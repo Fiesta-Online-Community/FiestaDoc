92cfcf14827f55f46c586f9aafaf125ad73c107b
Daniel Engelke <flainity@gmail.com>
2022-06-10 16:14:27 +0200

    Initial commit

---
+++ sidebars.js
@@ -0,0 +1,16 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+    main: [
+        'introduction'
+    ],
+    shine: [
+        'shine-files/getting-started',
+        { 'File List':
+            [
+            'shine-files/abstate'
+            ]
+        }
+    ]
+};
+
+module.exports = sidebars;
